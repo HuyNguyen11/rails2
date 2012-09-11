ec9088b44f244323cc94f0bc1827f9c62965e258
Huy <nqhuy91bk@gmail.com>
2012-09-11 15:38:58 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rail21::Application.config.secret_token = 'e9e920fe94e481679ca2bf2dc1e7e8e3dbb3cecaf2ae887a929bdaf7b7416df6ee968c904c85444c3fadb96f4c3a62a247a325f9498c393f9597d33453fe85a2'
